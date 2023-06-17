1a077d47950c14237c8d7a90e8476e223796a211
Shivam Raj <48693624+shivamraj74@users.noreply.github.com>
2023-06-17 20:26:39 +0530

    Create Contact.java

---
+++ src/main/java/com/example/contact/entities/Contact.java
@@ -0,0 +1,61 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private Long linkedId;
+
+    private String linkPrecedence;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getLinkedId() {
+        return linkedId;
+    }
+
+    public void setLinkedId(Long linkedId) {
+        this.linkedId = linkedId;
+    }
+
+    public String getLinkPrecedence() {
+        return linkPrecedence;
+    }
+
+    public void setLinkPrecedence(String linkPrecedence) {
+        this.linkPrecedence = linkPrecedence;
+    }
+}
